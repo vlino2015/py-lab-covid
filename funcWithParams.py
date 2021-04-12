69029a9c6a51e7fdf4be2d575aa8a3dec85ff8e6
Veronica Lino <veronica@digitalCrafts.com>
2021-04-12 13:35:10 -0500

    labs

---
+++ funcWithParams.py
@@ -0,0 +1,37 @@
+# Functions with parameters
+# Create a function that creates the following recommendation letter.
+# The Parameters for the functions should be the first and last name person you
+# are recommending
+
+# Karen Jones
+# 1234 Park St
+# Anytown, Pennsylvania 12345
+
+# April 14, 2019
+
+# ABC College Admission’s Board
+# 1234 Butler Ave
+# Everywhere, CA 12345
+
+# Dear ABC College Admission’s Board:
+
+# My name is Karen Jones. I have served as a science teacher at Parktown High School for the past fifteen years and have had the privilege to serve as Ryan Thomas’s teacher for the past three. I have also been Ryan’s advisor on the science academic team here at school. Due to his qualifications, I feel that Ryan would be an excellent addition to your school.
+
+# While he has been a student here, Ryan has always challenged himself academically, taking all of the AP courses that our school has to offer. He has been captain of the academic team for the past two years, showing strong leadership qualities and organizational skills. His superior written and verbal skills have far surpassed any student of his age.
+
+# Ryan would bring much to your school, both in and out of the classroom. If you have any questions regarding Ryan’s qualifications, please contact me at (123) 555-5555 or at Karen.Jones@email.com.
+
+# Sincerely,
+
+
+# Karen Jones
+# Science Department Head
+# Park Town High School
+
+
+
+
+#######################################################
+# Order of parameters
+# In the last example, reverse the order of the arguments when the function is
+# called (switch first name and last name) and look at the results
